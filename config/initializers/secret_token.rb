50bf3136a63d594eb69c8712f157a8a5b612c8b4
Alexis.Benavente <lxsbenavente@gmail.com>
2012-01-30 21:11:32 -0500

    Test primera subida Chat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DewChat::Application.config.secret_token = '088e78300a4d4c3fc8aeef04046d7b7f3af7b932078e2f92e86103bd06823523b03fbdd25213eb7f053d83bed71c33c032b2561a7d8e46cc3a311a9f9d49a64a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DewChat::Application.config.session_store :cookie_store, key: '_Dew_chat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DewChat::Application.config.session_store :active_record_store
